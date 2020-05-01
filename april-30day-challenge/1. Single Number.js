15e5fcab5a22e4b172e1258f18463fe44a2a6891
Paul <paul.constantin.chitic@gmail.com>
2020-05-01 09:43:15 +0300

    rename

--- april-30day-challenge/1. Single Number.js
+++
@@ -1,8 +0,0 @@
-// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
-
-// singleNumber :: [Number] -> Number
-const singleNumber = nums => {
-    let cache = new Set();
-    nums.forEach(n => { cache.has(n) ? cache.delete(n) : cache.add(n); });
-    return cache.values().next().value;
-};
