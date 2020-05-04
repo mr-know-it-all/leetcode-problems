68a35f6d4f8b547a637b66a17c8dcab262a2bace
Paul <paul.constantin.chitic@gmail.com>
2020-05-04 10:48:25 +0300

    may day 4

--- may-challenge-2020/04. Number Complement.js
+++ may-challenge-2020/04. Number Complement.js
@@ -2,9 +2,26 @@
 
 // Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
 
+// SOLUTION 1:
+
 // findComplement :: Number -> Number
 const findComplement = num => {
     const bitLength = num.toString(2).length;
 
     return ~num & (Math.pow(2, bitLength) - 1);
 };
+
+// SOLUTION 2:
+// may be more intuitive, because we flip one bit at a time
+
+// findComplement :: Number -> Number
+const findComplement = num => {
+  const complementString = (
+    num
+      .toString(2)
+      .split('')
+      .reduce((acc, val) => acc + (val ^ 1), '')
+  );
+
+  return parseInt(complementString, 2);
+}
