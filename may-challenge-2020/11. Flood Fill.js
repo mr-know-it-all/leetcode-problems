6320209c43ed52d1e0e068b7abec8a869768b6e6
Paul <paul.constantin.chitic@gmail.com>
2020-05-11 11:09:48 +0300

    may update

---
+++ may-challenge-2020/11. Flood Fill.js
@@ -0,0 +1,36 @@
+// An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+
+// Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+// To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+
+// At the end, return the modified image.
+
+
+// SOLUTION 1:
+// TODO: Add more efficient solution
+
+// floodFill :: ([[Number]], Number, Number, Number) -> [[Number]]
+const floodFill = (image, x, y, newColor) => {
+    const startColor = image[x][y];
+    const visited = {};
+
+    const fill = (x, y) => {
+        if(visited[`${x}-${y}`]) return;
+        visited[`${x}-${y}`] = true;
+
+        if(image[x][y] === startColor) {
+            image[x][y] = newColor;
+
+            if(x + 1 < image.length) fill(x + 1, y); // DOWN
+            if(x - 1 >= 0) fill(x - 1, y); // UP
+            if(y + 1 < image[0].length) fill(x, y + 1); // RIGHT
+            if(y - 1 >= 0) fill(x, y - 1); // LEFT
+        }
+
+    }
+
+    fill(x, y);
+
+    return image;
+};
