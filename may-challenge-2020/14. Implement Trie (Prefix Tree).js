4865d824567c7fd1b8cd867f831dba1934e8c60f
Paul <paul.constantin.chitic@gmail.com>
2020-05-14 16:13:23 +0300

    may update

--- may-challenge-2020/14. Implement Trie (Prefix Tree).js
+++ may-challenge-2020/14. Implement Trie (Prefix Tree).js
@@ -6,19 +6,19 @@ function Trie() {
 
     // find :: (String, Boolean) -> Boolean
     this.find = function(word, strict) {
-        // we'll start at the root of the trie
+        // we'll start ar the root of the trie
         let currentNode = this.root;
 
         // for each i'th letter
         for(let i = 0; i < word.length; i++) {
             // compute the key as for insertion
-            let key = word.substr(0, i + 1);
+            let key = word[i] // word.substr(0, i + 1); (alterrnative B)
 
             // if there is no path in the trie, return false
             if(!currentNode.children[key]) return false;
 
             // if we have the complete match
-            if(key === word) {
+            if(i === word.length - 1) { // key === word (alterrnative B)
                 // check if the word is complete
                 if(strict && !currentNode.children[key].endWord) return false;
 
@@ -36,8 +36,8 @@ function Trie() {
 
         // for each i'th letter
         for(let i = 0; i < word.length; i++) {
-            // the key for a letter will be the word up to that letter inclusive
-            const key = word.substr(0, i + 1);
+            // the key for a letter can be the letter or the word up to that letter inclusive
+            const key = word[i] // word.substr(0, i + 1); (alterrnative B)
 
             // if the current node has this key as a descendant
             // we'll mark it if it's a complete word
