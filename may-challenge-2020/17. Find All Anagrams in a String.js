86c33595e2192c95c073546e5177288ba06aca66
Paul <paul.constantin.chitic@gmail.com>
2020-05-17 15:53:13 +0300

    may update

--- may-challenge-2020/17. Find All Anagrams in a String.js
+++ may-challenge-2020/17. Find All Anagrams in a String.js
@@ -5,40 +5,33 @@
 // The order of output does not matter.
 
 // SOLUTION 1:
-// TODO: find faster solution, this one barely passes the tests
+// TODO: find faster solution:
 
 // findAnagrams :: (String, String) -> [Number]
 const findAnagrams = function(s, p) {
     const result = [];
-
-    const hash = {};
     const wordLen = p.length;
-    for(let i = 0; i < wordLen; i++) {
-        const char = p[i];
-        hash[char] = (hash[char] || 0) + 1;
-    }
-
     const stringLen = s.length;
-    for(let i = 0; i + wordLen <= stringLen; i++) {
-        let found = true;
-        const localHash = Object.assign({}, hash);
 
+    const hash = Array.from({ length: 26 }, () => 0);
+    for(let i = 0; i < wordLen; i++) hash[p.charCodeAt(i) - 97]++;
+
+    // for every possible anagram stating from index i
+    forEveryPossibleIndex:
+    for(let i = 0; i + wordLen <= stringLen; i++) {
+        // make a local copy of the characters hash
+        const localHash = hash.slice();
+
+        // check if all characters in this window (i -> wordSize) are in hash
         const wordLimit = i + wordLen;
         for(let j = i; j < wordLimit; j++) {
-            const char = s[j];
-            if(localHash[char] === undefined || localHash[char] === -1) {
-                found = false;
-                break;
-            }
-
-            if(localHash[char] === 1) {
-                localHash[char] = -1;
-            } else {
-                localHash[char] -= 1;
-            }
+            const char = s.charCodeAt(j) - 97;
+            // if a character is missing it means this is not a valid starting index
+            if(localHash[char] === 0) continue forEveryPossibleIndex;
+            localHash[char] -= 1;
         }
 
-        if(found) result.push(i);
+        result.push(i);
     }
 
     return result;
