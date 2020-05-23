c967f844cdcb9cf6c206da64ff877c914709c360
Paul <paul.constantin.chitic@gmail.com>
2020-05-23 12:55:47 +0300

    may day 23

--- may-challenge-2020/23. Interval List Intersections.js
+++ may-challenge-2020/23. Interval List Intersections.js
@@ -5,7 +5,6 @@
 // (Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
 
 // SOLUTION 1:
-// TODO: find faster solution than O(n**m)
 
 // intervalIntersection :: ([[Number]], [[Number]]) -> [[Number]]
 const intervalIntersection = function(A, B) {
@@ -28,6 +27,28 @@ const intervalIntersection = function(A, B) {
 
             intersection && result.push(intersection)
         }
+    }
+
+    return result;
+};
+
+// SOLUTION 2:
+
+// intervalIntersection :: ([[Number]], [[Number]]) -> [[Number]]
+const intervalIntersection = function(A, B) {
+    let result = [];
+
+    let a = 0;
+    let b = 0;
+
+    while(a < A.length && b < B.length) {
+        const maxLeft = Math.max(A[a][0], B[b][0]);
+        const minRight = Math.min(A[a][1], B[b][1]);
+
+        if(maxLeft <= minRight) result.push([maxLeft, minRight]);
+
+        if(minRight === A[a][1]) a++;
+        else b++;
     }
 
     return result;
