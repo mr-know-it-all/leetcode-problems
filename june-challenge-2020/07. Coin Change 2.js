9dbb2f2b312960ac5c0bf6de93b3f4f19b67c11b
Paul <paul.constantin.chitic@gmail.com>
2020-06-07 13:16:30 +0300

    june update

--- june-challenge-2020/07. Coin Change 2.js
+++ june-challenge-2020/07. Coin Change 2.js
@@ -1,5 +1,25 @@
 // You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
 
+// SOLUTON 1:
+// (TLE but intuitive)
+
+// change :: (Number, [[Number]]) -> Number
+const change = (amount, coins) => {
+    if(amount === 0) return 1;
+    if(amount < 0 || coins.length === 0) return 0;
+
+    let copy = coins.slice(0);
+
+    return (
+      // we use a coin
+      change(amount - copy.pop(), coins) +
+      // we discard it
+      change(amount, copy)
+    );
+};
+
+// SOLUTION 2:
+
 // change :: (Number, [[Number]]) -> Number
 const change = (amount, coins) => {
     let dp = Array.from({ length: amount + 1 }, () => 0);
