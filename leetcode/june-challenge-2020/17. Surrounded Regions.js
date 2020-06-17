3426026cf50d91c8219afa5d9fafb864d1095db4
Paul <paul.constantin.chitic@gmail.com>
2020-06-18 01:20:56 +0300

    june update

--- leetcode/june-challenge-2020/17. Surrounded Regions.js
+++ leetcode/june-challenge-2020/17. Surrounded Regions.js
@@ -20,21 +20,21 @@ const solve = board => {
         if(j > 1 && board[i][j - 1] === O) exclude(i, j - 1);
     };
 
-	for (let i = 0; i < m; i++) {
-		if(board[i][0] === O) exclude(i, 0);
-		if(board[i][n - 1] === O) exclude(i, n - 1);
-	}
+  	for(let i = 0; i < m; i++) {
+  		if(board[i][0] === O) exclude(i, 0);
+  		if(board[i][n - 1] === O) exclude(i, n - 1);
+  	}
 
-	for (let j = 0; j < n; j++) {
-		if(board[0][j] === O) exclude(0, j);
-		if(board[m - 1][j] === O) exclude(m - 1, j);
-	}
+  	for(let j = 0; j < n; j++) {
+  		if(board[0][j] === O) exclude(0, j);
+  		if(board[m - 1][j] === O) exclude(m - 1, j);
+  	}
 
-	for (let i = 0; i < m; i++) {
-		for (let j = 0; j < n; j++) {
-			if(board[i][j] === O) board[i][j] = X;
-			else if(board[i][j] === EXCLUDED) board[i][j] = O;
-		}
-	}
+  	for(let i = 0; i < m; i++) {
+  		for (let j = 0; j < n; j++) {
+  			if(board[i][j] === O) board[i][j] = X;
+  			else if(board[i][j] === EXCLUDED) board[i][j] = O;
+  		}
+  	}
 
 };
