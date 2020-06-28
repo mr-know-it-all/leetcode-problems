b0209d91d4e1bd8163578837f8f8b22b29abb4bb
Paul <paul.constantin.chitic@gmail.com>
2020-06-28 08:54:18 +0300

    june update

---
+++ leetcode/june-challenge-2020/27. Perfect Squares.js
@@ -0,0 +1,29 @@
+// Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n
+
+const isSquare = n => Math.sqrt(n) % 1 === 0;
+
+// numSquares :: Number -> Number
+var numSquares = function(n) {
+    const cache = {};
+    let result = Infinity;
+
+    (function decompose(n, count = 0) {
+        if(n < 0 || count >= result) return;
+
+        if(cache[n + '-' + count]) return;
+        cache[n + '-' + count] = true;
+
+        if(n === 0) {
+            result = Math.min(result, count);
+            return;
+        }
+
+        let next = n;
+        while(next > 0) {
+            if(isSquare(next)) decompose(n - next, count + 1);
+            next--;
+        }
+    })(n);
+
+    return result;
+};
