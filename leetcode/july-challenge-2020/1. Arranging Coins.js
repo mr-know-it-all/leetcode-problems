f4011a93f9c474e34c1a94fb8876b5819b506779
Paul <paul.constantin.chitic@gmail.com>
2020-07-01 13:25:58 +0300

    july update

--- leetcode/july-challenge-2020/1. Arranging Coins.js
+++ leetcode/july-challenge-2020/1. Arranging Coins.js
@@ -26,9 +26,41 @@
 
 // Because the 4th row is incomplete, we return 3.
 
+// SOLUTION 1:
+// (slow but suprisingly not TLE)
+
 // arrangeCoins :: Number -> Number
 const arrangeCoins = n => {
     let stairs = 0;
     while(n >= stairs) n -= stairs++;
     return stairs - 1;
+};
+
+// SOLUTION 2:
+// inspired from the official solution:
+
+// [...]
+// Assume that the answer is k, i.e. we've managed to complete k rows of coins.
+// These completed rows contain in total 1 + 2 + ... + k = (k * (k + 1)) / 2 coins.
+// [...]
+
+// arrangeCoins :: Number -> Number
+const arrangeCoins = n => {
+    let start = 0;
+    let end = n;
+
+    let mid;
+    let val;
+
+    while(start <= end) {
+        mid = (start + end) >> 1;
+        val = (mid * (mid + 1)) / 2;
+
+        if(val === n) return mid;
+
+        if(n < val) end = mid - 1;
+        else start = mid + 1;
+    }
+
+    return end;
 };
