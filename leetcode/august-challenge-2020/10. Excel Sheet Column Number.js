5e9eade0d99f98e428c46399b2a4fad0f93c7dd6
Paul <paul.constantin.chitic@gmail.com>
2020-08-10 19:46:37 +0300

    august update

--- leetcode/august-challenge-2020/10. Excel Sheet Column Number.js
+++ leetcode/august-challenge-2020/10. Excel Sheet Column Number.js
@@ -1,11 +1,29 @@
 // Given a column title as appear in an Excel sheet, return its corresponding column number.
 
+// SOLUTION 1:
+
 // titleToNumber :: String -> Number
 const titleToNumber = function(s) {
     let total = 0;
 
     for(let i = 0; i < s.length; i++) {
         total += (s.charCodeAt(i) - 64) * Math.pow(26, s.length - i - 1);
+    }
+
+    return total;
+};
+
+
+// SOLUTION 2:
+
+// titleToNumber :: String -> Number
+const titleToNumber = function(s) {
+    let total = 0;
+    let multiplier = 1;
+
+    for(let i = s.length - 1; i >= 0; i--) {
+        total += (s.charCodeAt(i) - 64) * multiplier;
+        multiplier *= 26;
     }
 
     return total;
