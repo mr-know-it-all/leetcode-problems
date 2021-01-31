bf8cba4290a97492d431eb6f06d77b2519310e9f
Paul <paul.constantin.chitic@gmail.com>
2021-01-31 09:01:59 +0200

    updates

--- leetcode/january-challenge-2021/30. Minimize Deviation in Array.js
+++ leetcode/january-challenge-2021/30. Minimize Deviation in Array.js
@@ -37,7 +37,44 @@
 // SOLUTION 1:
 // TODO: use priority queue or maxHeap to reduce complexity
 
-// TODO: use priority queue
+// You are given an array nums of n positive integers.
+
+// You can perform two types of operations on any element of the array any number of times:
+
+// If the element is even, divide it by 2.
+// For example, if the array is [1,2,3,4], then you can do this operation on the last element, and the array will be [1,2,3,2].
+// If the element is odd, multiply it by 2.
+// For example, if the array is [1,2,3,4], then you can do this operation on the first element, and the array will be [2,2,3,4].
+// The deviation of the array is the maximum difference between any two elements in the array.
+
+// Return the minimum deviation the array can have after performing some number of operations.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: 1
+// Explanation: You can transform the array to [1,2,3,2], then to [2,2,3,2], then the deviation will be 3 - 2 = 1.
+// Example 2:
+
+// Input: nums = [4,1,5,20,3]
+// Output: 3
+// Explanation: You can transform the array after two operations to [4,2,5,5,3], then the deviation will be 5 - 2 = 3.
+// Example 3:
+
+// Input: nums = [2,10,8]
+// Output: 3
+ 
+
+// Constraints:
+
+// n == nums.length
+// 2 <= n <= 105
+// 1 <= nums[i] <= 109
+
+// SOLUTION 1:
+
 // addToQueue :: ([Number], Number) -> ()
 const addToQueue = (q, n) => {
     for(let i = 0; i < q.length; i++) {
@@ -69,23 +106,39 @@ const minimumDeviation = (nums) => {
     // TODO: if it were a priority queue this would not be needed
     q.sort((a, b) => b - a);
     
+    res = Math.min(res, q[0] - min);
+    
     // while largest number in the queue is even, meaning can be decreased
     while(q[0] % 2 === 0) {
         // remove largest element
         const max = q.shift();
-        
-        // update response
-        res = Math.min(res, max - min);
-        
+                
         const decreased = max / 2;
         
         // decreased may be less than current min
         min = Math.min(min, decreased);
-        
         // add decreased value to queue
         addToQueue(q, decreased);
+        
+        // update response
+        res = Math.min(res, q[0] - min);
     }
     
-    // make one last check for res before returning it
-    return Math.min(res, q[0] - min);
+    return res;
+};
+
+// SOLUTION 2:
+
+// minimumDeviation :: [Number] -> Number
+const minimumDeviation = function(nums) {
+    const q = new MaxPriorityQueue({ priority: x => x });
+    for(let n of nums) q.enqueue(n % 2 ? n * 2 : n);
+    
+    let ans = q.front().element - q.back().element;
+    while(q.front().element % 2 === 0) {a
+        q.enqueue(q.dequeue().element / 2);
+        ans = Math.min(ans, q.front().element - q.back().element);
+    }
+    
+    return ans;
 };
